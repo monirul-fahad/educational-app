7493f0b7a0ea7d36501d65318b60787e7a34f49e
Monirul Fahad <immonirulfahad@gmail.com>
2021-10-04 23:00:43 +0600

    final

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
-import Services from "./components/Services/Services";
 
 function App() {
   return (
